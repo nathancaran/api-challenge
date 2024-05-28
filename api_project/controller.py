118a8a1695395a775abcbca69ccf9cfea89dd686
nathancaran <caran494@hsd.k12.or.us>
2024-05-28 09:50:36 -0700

    Adding pyqt6 stuff

--- api_project/controller.py
+++ api_project/controller.py
@@ -1,3 +1,35 @@
+import sys
+from tkinter.ttk import Widget
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QHBoxLayout,
+                             QVBoxLayout,QLabel,QPushButton,QSpinBox,QLineEdit,QDoubleSpinBox,
+                             QCheckBox,QComboBox,QListWidget)
+from PyQt6.QtWidgets import ( QMainWindow, QApplication, QVBoxLayout, QHBoxLayout,
+    QLabel, QCheckBox, QComboBox, QListWidget, QTextEdit,
+    QSpinBox, QDoubleSpinBox, QSlider, QWidget, QPushButton,)
+from PyQt6.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+
+        self.setWindowTitle("Random Genre Generator")
+
+        # Main layout ####################
+        main_layout = QHBoxLayout()
+
+
+        # Left and right panes #############
+        top_pane = QVBoxLayout()
+        left_pane = QVBoxLayout()
+
+        title_label = QLabel("Random Genre Generator")
+
+
+
 
 import requests as re
 
@@ -9,3 +41,11 @@ if response.ok:
     print(response.text)
 else:
     print(f"There was an error: {response.status_code}")
+
+    app = QApplication(sys.argv)
+
+
+window = MainWindow()
+window.show()
+    
+app.exec()
